b7809bb064861d9f73f000e5ee340029502fe1e3
Anthony <anthonyharisson30@gmail.com>
2022-11-18 23:32:28 -0300

    first commit

--- App.js
+++ App.js
@@ -1,20 +1,33 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react';
+import { StyleSheet, View } from 'react-native';
+import { Button, Input, Text } from 'react-native-elements';
+import Icon  from 'react-native-vector-icons/FontAwesome';
+import { createStackNavigator } from '@react-navigation/stack';
+import { NavigationContainer } from '@react-navigation/native';
+import Login from './screens/Login/Login';
+import Principal from './screens/Principal/Principal';
 
-export default function App() {
+const Stack = createStackNavigator();
+
+function MyStack() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <Stack.Navigator
+    screenOptions={{
+      headerShown: false
+    }}
+    >
+      <Stack.Screen name="Login" component={Login} />
+      <Stack.Screen name="Principal" component={Principal} />
+    </Stack.Navigator>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default function App() {
+
+  return (
+    <NavigationContainer>
+      <MyStack />
+    </NavigationContainer>
+  );
+}
+
---
+++ screens/Login/Login.js
@@ -0,0 +1,42 @@
+import { useState } from 'react';
+import { StyleSheet, View } from 'react-native';
+import { Button, Input, Text } from 'react-native-elements';
+import Icon  from 'react-native-vector-icons/FontAwesome';
+import styles from '../../Styles/LoginStyles';
+
+
+export default function Login({navigation}) {
+
+  const [email, setEmail] = useState(null)
+  const [password, setPassword] = useState(null)
+
+  const entrar = () => {
+    navigation.reset({
+        index: 0,
+        routes: [{name: "Principal"}]
+    })
+  }
+
+  return (
+    <View style={styles.loginContainer}>
+      <Text h3>Entre no TemTudoAki</Text>
+      <Input placeholder='E-mail'
+      keyboardType='email-address'
+      leftIcon={{type: 'font-awesome', name: 'envelope'}}
+      onChangeText={value => setEmail(value)}
+      />
+      <Input placeholder='Password'
+      leftIcon={{type: 'font-awesome', name: 'lock'}}
+      onChangeText={value => setPassword(value)}
+      secureTextEntry={true}
+      />
+      <Button 
+      icon={<Icon name={'check'} size={15} color='white' />}
+      title='Entrar'
+      onPress={() => entrar()}
+      />
+      
+    </View>
+  );
+}
+
